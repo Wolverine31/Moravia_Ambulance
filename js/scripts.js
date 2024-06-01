f7b9edf3c6d53f7349388cdad0541137abbec3d7
Alex Bezrodnyi <o.bezrodnyi@gmail.com>
2024-06-01 14:26:30 +0200

    add Team page

--- js/scripts.js
+++ js/scripts.js
@@ -46,18 +46,47 @@ $(document).ready(function(){
   });
 
 // Accordion
-const items = document.querySelectorAll(".accordion button");
+// const items = document.querySelectorAll(".accordion button");
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
+// function toggleAccordion() {
+//   const itemToggle = this.getAttribute('aria-expanded');
   
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
+//   for (i = 0; i < items.length; i++) {
+//     items[i].setAttribute('aria-expanded', 'false');
+//   }
   
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
+//   if (itemToggle == 'false') {
+//     this.setAttribute('aria-expanded', 'true');
+//   }
+// }
 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+// items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+// calendar
+// document.addEventListener('DOMContentLoaded', function() {
+//     var calendarEl = document.getElementById('calendar');
+//     var modal = document.getElementById("modalForm");
+//     var span = document.getElementsByClassName("close")[0];
+
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//         initialView: 'dayGridMonth',
+//         dateClick: function(info) {
+//             document.querySelector('iframe').src = "https://docs.google.com/forms/d/e/ВАШ_СЕКРЕТНЫЙ_КОД/viewform?embedded=true&entry.DATE_FIELD=" + info.dateStr;
+//             modal.style.display = "block";
+//         }
+//     });
+
+//     calendar.render();
+
+//     // Закрытие модального окна при нажатии на X
+//     span.onclick = function() {
+//         modal.style.display = "none";
+//     }
+
+//     // Закрытие модального окна при нажатии вне его
+//     window.onclick = function(event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     }
+// });
